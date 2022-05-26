46196ab0ae705cef27d3e9305e5c0edac00cc6ac
mcbrunette33 <mcbrunette33@gmail.com>
2022-05-25 17:23:48 -0700

    finished lab8

---
+++ app/src/main/java/quotes/Quotes.java
@@ -0,0 +1,11 @@
+package quotes;
+
+public class Quotes {
+    public String author;
+    public String text;
+
+    public Quotes(String author, String text) {
+        this.author = author;
+        this.text = text;
+    }
+}
